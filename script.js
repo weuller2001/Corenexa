3c475a8ad90054c601f7a2b3af410808fbb51879
weuller2001 <weuller.santos@keevo.com.br>
2025-12-10 21:50:28 -0300

    Alteração no visual da api de email

--- script.js
+++ script.js
@@ -28,3 +28,48 @@ if (mobileMenuIcon && navLinks) {
         navLinks.classList.toggle('active');
     });
 }
+
+/* --- Lógica de Envio de Formulário (AJAX) --- */
+const form = document.getElementById("my-form");
+
+if (form) {
+    
+    async function handleSubmit(event) {
+        event.preventDefault(); // Impede o site de mudar de página
+        
+        const status = document.getElementById("my-form-status");
+        const data = new FormData(event.target);
+        
+        // Envia os dados para o Formspree em segundo plano
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                // Sucesso!
+                status.innerHTML = "Obrigado! Sua mensagem foi enviada com sucesso.";
+                status.classList.add('success'); // Fica verde
+                form.reset(); // Limpa os campos
+            } else {
+                // Erro do servidor
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                    } else {
+                        status.innerHTML = "Oops! Ocorreu um erro ao enviar.";
+                    }
+                    status.classList.add('error'); // Fica vermelho
+                })
+            }
+        }).catch(error => {
+            // Erro de conexão
+            status.innerHTML = "Oops! Houve um problema na conexão.";
+            status.classList.add('error');
+        });
+    }
+
+    form.addEventListener("submit", handleSubmit);
+}
